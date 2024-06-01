154e92be46e6da2125cef5023f5d19ae7467a1f5
MakineniSuryaTej <64590688+MakineniSuryaTej@users.noreply.github.com>
2024-06-01 16:50:44 -0500

    Create increasingTriplet.py

---
+++ 334. Increasing Triplet Subsequence/increasingTriplet.py
@@ -0,0 +1,11 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        n1 = n2 = float('inf')
+        for i in nums:
+            if i <= n1:
+                n1 = i
+            elif i <= n2:
+                n2 = i
+            else:
+                return True
+        return False
